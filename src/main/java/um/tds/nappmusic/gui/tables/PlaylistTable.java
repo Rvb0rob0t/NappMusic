4243dd375b3c9c3397d4c53fd63e5e45b7696a68
useredsa <emilio.dominguezs@um.es>
2020-12-08 22:33:55 +0100

    New Playlist now adds the song selected

--- src/main/java/um/tds/nappmusic/gui/tables/PlaylistTable.java
+++ src/main/java/um/tds/nappmusic/gui/tables/PlaylistTable.java
@@ -7,6 +7,7 @@ import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPopupMenu;
 import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
 import javax.swing.SwingUtilities;
 import um.tds.nappmusic.controller.Controller;
 import um.tds.nappmusic.domain.Playlist;
@@ -22,6 +23,7 @@ public class PlaylistTable extends MouseAdapter {
 
   public PlaylistTable(MusicPlayer musicPlayer, Playlist playlist, JPopupMenu rightClickMenu) {
     this.table = new JTable(new PlaylistTableModel(playlist));
+    this.table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
     this.table.addMouseListener(this);
     this.musicPlayer = musicPlayer;
 
@@ -32,7 +34,13 @@ public class PlaylistTable extends MouseAdapter {
         e -> {
           String newPlaylistName = JOptionPane.showInputDialog("Name of the new playlist");
           if (newPlaylistName != null && newPlaylistName.length() > 0) {
-            Controller.getSingleton().createPlaylist(newPlaylistName);
+            int row = table.getSelectedRow();
+            if (row != -1) {
+              Controller controller = Controller.getSingleton();
+              Playlist created = controller.createPlaylist(newPlaylistName);
+              Song song = playlist.getSong(row);
+              controller.addToPlaylist(created, song);
+            }
           }
         });
     this.addToPlaylistMenu.add(newPlaylistItem);
@@ -57,17 +65,20 @@ public class PlaylistTable extends MouseAdapter {
     Playlist playlist =
         ((PlaylistTableModel) table.getModel()).getPlaylist(); // TODO Check the table model type
     int row = table.rowAtPoint(e.getPoint());
-    Song song = playlist.getSong(row);
-
-    if (SwingUtilities.isLeftMouseButton(e)) {
-      musicPlayer.play(playlist, row);
-    } else if (SwingUtilities.isRightMouseButton(e)) {
-      addAllPlaylistsAsItems(song);
-      rightClickMenu.show(e.getComponent(), e.getX(), e.getY());
+    if (row != -1) {
+      Song song = playlist.getSong(row);
+      if (SwingUtilities.isLeftMouseButton(e)) {
+        musicPlayer.play(playlist, row);
+      } else if (SwingUtilities.isRightMouseButton(e)) {
+        // Set the song as selected to operate on it with new lists
+        table.setRowSelectionInterval(row, row);
+        addAllPlaylistsAsMenuItems(song);
+        rightClickMenu.show(e.getComponent(), e.getX(), e.getY());
+      }
     }
   }
 
-  private void addAllPlaylistsAsItems(Song song) {
+  private void addAllPlaylistsAsMenuItems(Song song) {
     addToPlaylistMenu.removeAll();
     Controller.getSingleton()
         .getUserPlaylists()
